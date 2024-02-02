5bcd6a947f5a64e0326ea579014efca0329fe48b
Itserge1 <sergecodefirst@gmail.com>
2024-02-01 21:30:53 -0500

    Fixed param error

--- myscript.groovy
+++ myscript.groovy
@@ -1,13 +1,13 @@
 def buildApp() {
-    echo "Building application..."
+    echo "Building the application..."
 }
 
 def testApp() {
-    echo "Testing application..."
+    echo "Testing the application..."
 }
 
 def deployApp() {
     echo 'Deploying the application...'
-    echo "Deploying the application version ${param.VERSION}"
+    echo "Deploying the application version ${params.VERSION}"
 }
 return this
